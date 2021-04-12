3b5bdcae41d74e20fd3921dbd724906e8891f8b2
Luca De Angeli <lu.deangeli@gmail.com>
2021-04-12 01:10:32 +0100

    adds fighter interfaces and related ones

---
+++ enums/shared.enum.ts
@@ -0,0 +1,35 @@
+export enum Tribe {
+  hemi = "hemi",
+  theri = "theri",
+  xana = "xana",
+}
+
+export enum Sign {
+  air = "air",
+  earth = "earth",
+  fire = "fire",
+  lightning = "lightning",
+  water = "water",
+}
+
+export enum Class {
+  champ = "champ",
+  guru = "guru",
+  rogue = "rogue",
+  scout = "scout",
+  warlock = "warlcok",
+}
+
+export enum Rarity {
+  c = "c",
+  uc = "uc",
+  r = "r",
+  e = "e",
+  l = "l",
+}
+
+export enum skillLevel {
+  novice = "novice",
+  adept = "adept",
+  elite = "elite",
+}
