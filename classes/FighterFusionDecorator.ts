640084359eff23cee441f9b8ab167fc1c2a4f60a
Pedro Rougemont <Pedro Rougemont>
2021-05-15 13:47:35 -0300

    Reworking FighterFusion interface and decorator. Split the FighterFusion fields acroos Catalog/UserFighter interfaces and Changed the Decorator to decorate BandFighter instead

--- classes/FighterFusionDecorator.ts
+++ classes/FighterFusionDecorator.ts
@@ -1,22 +1,13 @@
-import { CatalogFighter, FighterFusion, Rarity, Stats, Tier } from "../interfaces/fighter.interface";
+import { BandFighter, Stats, Tier } from "../interfaces/fighter.interface";
 
 /**
- * Decorator for FighterFusion interface with calculations for both client and server 
- * side functionalities related to Fuse Fighter screen.
+ * Decorator for BandFighter interface with calculations for both client and server 
+ * side functionalities related to Fighter Fusion screen.
  */
-export default class FighterFusionDecorator {
-
-    // Fighter reference to be decorated.
-    private fighter : CatalogFighter;
+export default class BandFighterDecorator {
     
-    // Fighter's current SEF.
-    private currentSef : number;
-
-    // Fighter's total XP since level 0.
-    private totalXp : number;
-
-    // Fighter's evolution step 0, 1 or 2.
-    private evolutionStep: number;
+    // Band fighter
+    private bandFighter: BandFighter;
 
     // Tier-based min/max level limits.
     private tierLevels : number[];
@@ -26,15 +17,11 @@ export default class FighterFusionDecorator {
     
     // Reverse level progression formula, obtaining level from totalXp.
     private static levelProgressionReverse = (level : number) => { return Math.floor( Math.log( (level - 0.07) / 100 ) / (61/1250) ) };
-
-
-    constructor(fighterFusion : FighterFusion) {
-        this.fighter = fighterFusion.fighter;
-        this.currentSef = fighterFusion.currentSef;        
-        this.totalXp = fighterFusion.totalXp;
-        this.evolutionStep = fighterFusion.evolutionStep;        
+        
+    constructor(fighterFusion : BandFighter) {
+        this.bandFighter = fighterFusion;      
         // Extracts the array of min/max limits per evolutionStep from enum in the form: "tX_min1_min2_min3_min4_min5_min6". 
-        this.tierLevels = this.fighter.tier.substring("tX_".length).split("_").map(d => Number(d));
+        this.tierLevels = this.bandFighter.tier.substring("tX_".length).split("_").map(d => Number(d));
     }
 
     /**
@@ -44,22 +31,30 @@ export default class FighterFusionDecorator {
     public currentMaxLevel() {
         // Evolution step 0: consider tierLevels[0] and tierLevels[1]; 
         // evolution step 1: consider tierLevels[2] and tierLevels[3]...
-        var minIndex = 2 * this.evolutionStep;
-        var maxIndex = 2 * this.evolutionStep + 1;
-        return this.tierLevels[minIndex] + (this.currentSef) 
-                * ( ( this.tierLevels[maxIndex] - this.tierLevels[minIndex] ) / this.fighter.maxSef );
+        var minIndex = 2 * this.bandFighter.evolutionStep;
+        var maxIndex = 2 * this.bandFighter.evolutionStep + 1;
+        return this.tierLevels[minIndex] + (this.bandFighter.currentSef) 
+                * ( ( this.tierLevels[maxIndex] - this.tierLevels[minIndex] ) / this.bandFighter.maxSef );
     }
 
     /**
-     * Calculates xp requirements.
+     * Return current level based on unit's Xp.
+     * @returns level for current fighter
+     */
+    public currentLevel(): number {
+        return this.levelReachedAdding(0); //fodderXp = 0
+    }
+    
+    /**
+     * Calculates xp requirements for certain level.
      * @param level for which retrieve the xp requirement
      * @returns levelUpXp required for level
      */
-    public xpForLevel(level : number) {
+    public xpRequiredFor(level : number) {
         if( level > this.currentMaxLevel() || level < 0 ) throw new Error("Invalid level for current Fighter");
         // It can be linked later to some fighter characteristic such as Rarity or Tier.
         // Right now, levelProgression is always the same formula.
-        return FighterFusionDecorator.levelProgression(level);
+        return BandFighterDecorator.levelProgression(level);
     }
 
     /**
@@ -68,11 +63,11 @@ export default class FighterFusionDecorator {
      * @param fodderXp total xp selected for fusion.
      * @returns level which can be equal or higher than current fighter level
      */
-    public levelForXp(fodderXp : number) : number {
+    public levelReachedAdding(fodderXp : number) : number {
         // It can be linked later to some fighter characteristic such as Rarity or Tier.
-        // Right now, levelProgressionReverse is the reverse of a same formula.
+        // Right now, levelProgressionReverse is just the reverse of levelProgression formula.
         return Math.min(
-            FighterFusionDecorator.levelProgressionReverse(this.totalXp + fodderXp), 
+            BandFighterDecorator.levelProgressionReverse(this.bandFighter.totalXp + fodderXp), 
             this.currentMaxLevel()
         );
     }
@@ -85,14 +80,22 @@ export default class FighterFusionDecorator {
     public levelUpIncrements() : Stats {
         var steps = this.currentMaxLevel();
         return { 
-            hp : (this.fighter.maxStats.hp  - this.fighter.minStats.hp)  / steps,
-            atk: (this.fighter.maxStats.atk - this.fighter.minStats.atk) / steps,
-            def: (this.fighter.maxStats.def - this.fighter.minStats.def) / steps,
-            wis: (this.fighter.maxStats.wis - this.fighter.minStats.wis) / steps,
-            agi: (this.fighter.maxStats.agi - this.fighter.minStats.agi) / steps
+            hp : (this.bandFighter.maxStats.hp  - this.bandFighter.minStats.hp)  / steps,
+            atk: (this.bandFighter.maxStats.atk - this.bandFighter.minStats.atk) / steps,
+            def: (this.bandFighter.maxStats.def - this.bandFighter.minStats.def) / steps,
+            wis: (this.bandFighter.maxStats.wis - this.bandFighter.minStats.wis) / steps,
+            agi: (this.bandFighter.maxStats.agi - this.bandFighter.minStats.agi) / steps
         } as Stats;
     }
-    
+
+    /**
+     * Stats for current fighter level.
+     * @returns Stats for current fighter at level
+     */
+    public currentStats(): Stats {
+        return this.levelStats(this.currentLevel());
+    }
+
     /**
      * Stats for current fighter at the informed level.
      * @param level for which calculate the stats
@@ -102,11 +105,11 @@ export default class FighterFusionDecorator {
         if( level > this.currentMaxLevel() || level < 0 ) throw new Error("Invalid level for current Fighter");
         var increments = this.levelUpIncrements();
         return {
-            hp : this.fighter.minStats.hp  + Math.round(increments.hp  * level),
-            atk: this.fighter.minStats.atk + Math.round(increments.atk * level),
-            def: this.fighter.minStats.def + Math.round(increments.def * level),
-            wis: this.fighter.minStats.wis + Math.round(increments.wis * level),
-            agi: this.fighter.minStats.agi + Math.round(increments.agi * level),
+            hp : this.bandFighter.minStats.hp  + Math.round(increments.hp  * level),
+            atk: this.bandFighter.minStats.atk + Math.round(increments.atk * level),
+            def: this.bandFighter.minStats.def + Math.round(increments.def * level),
+            wis: this.bandFighter.minStats.wis + Math.round(increments.wis * level),
+            agi: this.bandFighter.minStats.agi + Math.round(increments.agi * level),
         } as Stats;
     }
     
@@ -114,49 +117,47 @@ export default class FighterFusionDecorator {
 
 function _test() {
     var fighterFusion = {
-        fighter: {
-            tier: Tier.t1_10_18_20_36_30_60,
-            minStats: {"hp": 59, "atk": 62, "def": 33, "wis": 52, "agi": 49},
-            maxStats: {"hp": 208, "atk": 220, "def": 108, "wis": 180, "agi": 168},
-            maxSef: 4
-        },
+        tier: Tier.t1_10_18_20_36_30_60,
+        minStats: {"hp": 59, "atk": 62, "def": 33, "wis": 52, "agi": 49},
+        maxStats: {"hp": 208, "atk": 220, "def": 108, "wis": 180, "agi": 168},
+        maxSef: 4,        
         totalXp: 100,
         currentSef: 0,
         evolutionStep: 0
-      } as FighterFusion;
+      } as BandFighter;
       
-      console.log(new FighterFusionDecorator(fighterFusion).currentMaxLevel());
+      console.log(new BandFighterDecorator(fighterFusion).currentMaxLevel());
 
       fighterFusion.currentSef = 4;
       
-      console.log(new FighterFusionDecorator(fighterFusion).currentMaxLevel());
-      console.log(new FighterFusionDecorator(fighterFusion).levelUpIncrements());
+      console.log(new BandFighterDecorator(fighterFusion).currentMaxLevel());
+      console.log(new BandFighterDecorator(fighterFusion).levelUpIncrements());
       
       //Should match:
-      console.log(fighterFusion.fighter.minStats);
-      console.log(new FighterFusionDecorator(fighterFusion).levelStats(0));
+      console.log(fighterFusion.minStats);
+      console.log(new BandFighterDecorator(fighterFusion).levelStats(0));
       
-      console.log(new FighterFusionDecorator(fighterFusion).levelStats(1));
-      console.log(new FighterFusionDecorator(fighterFusion).levelStats(17));
+      console.log(new BandFighterDecorator(fighterFusion).levelStats(1));
+      console.log(new BandFighterDecorator(fighterFusion).levelStats(17));
       
       // Should match:
-      console.log(new FighterFusionDecorator(fighterFusion).levelStats(18));      
-      console.log(fighterFusion.fighter.maxStats);
+      console.log(new BandFighterDecorator(fighterFusion).levelStats(18));      
+      console.log(fighterFusion.maxStats);
 
       // Changing evolutionStep for test purposes, other evolutionStep would have different 
       // min/max Stats, so don't test levelStats() and levelUpIncrements() from here.
       fighterFusion.evolutionStep = 2;
-      fighterFusion.fighter.maxSef = 5;
+      fighterFusion.maxSef = 5;
 
-      console.log(new FighterFusionDecorator(fighterFusion).xpForLevel(15));
-      console.log(new FighterFusionDecorator(fighterFusion).currentMaxLevel());
+      console.log(new BandFighterDecorator(fighterFusion).xpRequiredFor(15));
+      console.log(new BandFighterDecorator(fighterFusion).currentMaxLevel());
       
-      console.log(new FighterFusionDecorator(fighterFusion).levelForXp(107));
-      console.log(new FighterFusionDecorator(fighterFusion).levelForXp(108));
-      console.log(new FighterFusionDecorator(fighterFusion).levelForXp(109));
+      console.log(new BandFighterDecorator(fighterFusion).levelReachedAdding(107));
+      console.log(new BandFighterDecorator(fighterFusion).levelReachedAdding(108));
+      console.log(new BandFighterDecorator(fighterFusion).levelReachedAdding(109));
       
       fighterFusion.currentSef = 5;
       
-      console.log(new FighterFusionDecorator(fighterFusion).currentMaxLevel());
+      console.log(new BandFighterDecorator(fighterFusion).currentMaxLevel());
 }
 _test();
