b0e6f996d3a90919f309a9eca6662e77697207ae
Pedro Rougemont <Pedro Rougemont>
2021-07-26 13:44:20 -0300

    Adding a Login interface to represent the expected Response format from /login/:accessToken

--- interfaces/login.interface.ts
+++ interfaces/login.interface.ts
@@ -1,6 +1,13 @@
+import { Player } from "../../models/Player";
+
 export enum AuthenticationError {
     ACCESS_TOKEN_MISSING = "ACCESS_TOKEN_MISSING",
     ACCESS_TOKEN_EXPIRED = "ACCESS_TOKEN_EXPIRED",
     GOOGLE_AUTH_EXPIRED = "GOOGLE_AUTH_EXPIRED", 
     USER_NOT_FOUND = "USER_NOT_FOUND",    
+}
+export interface Login {
+    success: boolean,
+    message: AuthenticationError | Player,    
+    token: string | null
 }
