f4e1a68cbe2a92e26184ea63d26ad05991cde2be
Pedro Rougemont <Pedro Rougemont>
2021-07-27 12:08:56 -0300

    Error interface with HTTP error code and the multi-valued enum-like class HTTPError

--- interfaces/login.interface.ts
+++ interfaces/login.interface.ts
@@ -1,10 +1,12 @@
 import { Player } from "../../models/Player";
+import { HTTPError, HTTPErrorCode } from "./error.interface";
 
 export enum AuthenticationError {
     ACCESS_TOKEN_MISSING = "ACCESS_TOKEN_MISSING",
     ACCESS_TOKEN_EXPIRED = "ACCESS_TOKEN_EXPIRED",
     GOOGLE_AUTH_EXPIRED = "GOOGLE_AUTH_EXPIRED", 
-    USER_NOT_FOUND = "USER_NOT_FOUND",    
+    USER_NOT_FOUND = "USER_NOT_FOUND",
+    ERROR_CODE = 400
 }
 export interface Login {
     success: boolean,
